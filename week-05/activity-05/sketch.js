ee9ddbbac5af7d1a556afe417cfe8a984c4a70ed
Brian Clark <brian@brianclarkinteractive.com>
2022-10-19 16:14:36 -0700

    Week 5 » HW Round 9
    Updated canvas js line

--- week-05/activity-05/sketch.js
+++ week-05/activity-05/sketch.js
@@ -4,7 +4,8 @@ let xpos = 0;
 let ypos = 0;
 
 function setup() { 
-  createCanvas(400, 400);
+  var canvas = createCanvas(400, 400);
+  canvas.parent('sketch-frame');
 } 
 
 function draw() { 
@@ -32,13 +33,5 @@ function draw() {
     nextChange = millis() + timer;
     console.log(`time elapsed: ${round(millis() / 1000)}`);
   }
-  // Canvas frame
-  function setup() {
-    var canvas = createCanvas(400, 400);
-   
-    // Move the canvas so it’s inside our <div id="sketch-frame">.
-    canvas.parent('sketch-frame');
-  
-    background(255, 255, 255);
-  }
+
 }
